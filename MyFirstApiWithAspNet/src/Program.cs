c0bc55f9f572fbeb7309da4750466c80bc5d6ea2
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-07-18 17:04:59 -0400

    Securing connection string with environment variable

--- MyFirstApiWithAspNet/src/Program.cs
+++ MyFirstApiWithAspNet/src/Program.cs
@@ -2,7 +2,9 @@ using MyFirstApiWithAspNet.Endpoints.Categories;
 using MyFirstApiWithAspNet.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
+var connectionString = Environment.GetEnvironmentVariable("IWantDb");
+builder.Services.AddSqlServer<ApplicationDbContext>(connectionString);
+//builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
