830f497de21378bd1886af287a404c70b2c81dd6
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-08-08 15:16:02 -0400

    add endpoint OrderGet

---
+++ MyFirstApiWithAspNet/src/Endpoints/Orders/OrderGet.cs
@@ -0,0 +1,58 @@
+﻿namespace MyFirstApiWithAspNet.Endpoints.Oders;
+
+public class OrderGet
+{
+    public static string Template => "/orders";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static async Task<IResult> Action(ApplicationDbContext context, HttpContext http)
+    {
+        var clientIdClaim = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+        string clientCpfClaim = null;
+
+        var cpfClaim = http.User.Claims.FirstOrDefault(c => c.Type == "Cpf");
+
+        if (cpfClaim != null)
+        {
+            clientCpfClaim = cpfClaim.Value;
+        }
+
+        List<OrderResponse> orderResponses;
+
+        if (string.IsNullOrEmpty(clientCpfClaim))
+        {
+            orderResponses = await context.Orders
+                .Where(order => order.Products.Any())
+                .Join(context.Users,
+                    order => order.ClientId,
+                    user => user.Id,
+                    (order, user) => new OrderResponse(
+                        order.Id,
+                        user.UserName,
+                        order.DeliveryAddress,
+                        order.Products.Select(p => p.Name).ToList(),
+                        order.Total))
+                .ToListAsync();
+        }
+        else
+        {
+            orderResponses = await context.Orders
+                .Where(order => order.ClientId == clientIdClaim && order.Products.Any())
+                .Join(context.Users,
+                    order => order.ClientId,
+                    user => user.Id,
+                    (order, user) => new OrderResponse(
+                        order.Id,
+                        user.UserName,
+                        order.DeliveryAddress,
+                        order.Products.Select(p => p.Name).ToList(),
+                        order.Total))
+                .ToListAsync();
+        }
+
+        return Results.Ok(orderResponses);
+    }
+
+}
---
+++ MyFirstApiWithAspNet/src/Endpoints/Orders/OrderResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace MyFirstApiWithAspNet.Endpoints.Oders;
+
+public record OrderResponse(Guid Id, string ClientName, string DeliveryAddress, List<string> Products, decimal Total);
