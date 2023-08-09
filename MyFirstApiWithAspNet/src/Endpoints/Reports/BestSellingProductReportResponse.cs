fe7c763465fc1d109d8c53ca95574c0d2dc1c0ba
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-08-09 17:09:52 -0400

    add BestSellingProductReport

---
+++ MyFirstApiWithAspNet/src/Endpoints/Reports/BestSellingProductReportResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace MyFirstApiWithAspNet.Endpoints.Reports;
+
+public record BestSellingProductReportResponse(Guid Id, string ProductName, int TotalSold, decimal TotalRevenue);
+
---
+++ MyFirstApiWithAspNet/src/Infra/Data/QueryBestSellingProduct.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using MyFirstApiWithAspNet.Endpoints.Reports;
+
+namespace MyFirstApiWithAspNet.Infra.Data;
+
+public class QueryBestSellingProduct
+{
+    public async Task<IEnumerable<BestSellingProductReportResponse>> Execute(int page, int rows)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("IWantDb");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            Console.WriteLine("Variável de ambiente não encontrada nesse servidor!");
+            //return Results.BadRequest("Não autorizada a conexão com o banco de dados");
+        }
+
+        var query =
+          @"SELECT p.Id, p.Name AS ProductName, COUNT(op.ProductsId) AS TotalSold, SUM(pr.Price) AS TotalRevenue
+            FROM Products p
+            INNER JOIN OrderProducts op ON p.Id = op.ProductsId
+            INNER JOIN Orders o ON op.OrdersId = o.Id
+            INNER JOIN Products pr ON p.Id = pr.Id
+            GROUP BY p.Id, p.Name
+            ORDER BY TotalRevenue DESC";
+
+        using (var db = new SqlConnection(connectionString))
+        {
+            await db.OpenAsync();
+
+            var result = await db.QueryAsync<BestSellingProductReportResponse>(
+                query,
+                new { page, rows }
+            );
+
+            return result;
+        }
+    }
+}
